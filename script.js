37ec7a5bf02605f9b3cb34b0ad12d8ee7514de60
Biruk Jira G. <birukjira@gmail.com>
2024-12-22 18:17:44 +0300

    JavaScript code for guess my number game   using conditional statements   using functions   using event listeners   using DOM manipulation

---
+++ script.js
@@ -0,0 +1,83 @@
+"use strict";
+// selecting the elements
+const guessInput = document.querySelector(".guess");
+const checkBtn = document.querySelector(".check");
+const message = document.querySelector(".message");
+const scoreDisplay = document.querySelector(".score");
+const highScoreDisplay = document.querySelector(".highscore");
+const numberDisplay = document.querySelector(".number");
+const againBtn = document.querySelector(".again");
+
+// generating a random number between 1 and 20
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+
+// initial score value
+let score = 20;
+let highScore = 0;
+
+// adding event listener to check button
+checkBtn.addEventListener("click", () => {
+  // getting the user input
+  const userGuess = Number(guessInput.value);
+  if (score > 0) {
+    if (userGuess < 1 || userGuess > 20) {
+      // inform the user to enter a number between 1 and 20
+      message.textContent = "Please enter a number between 1 and 20";
+    } else if (userGuess > secretNumber) {
+      // inform the user if the guess is higher than the random number
+      message.textContent = "📈 Too high, try again";
+      // decreasing the score by 1
+      score--;
+      scoreDisplay.textContent = score;
+      if (score === 0) {
+        score = 0;
+        message.textContent =
+          "😭 You lost the game, the number was " + secretNumber;
+        scoreDisplay.textContent = 0;
+        document.body.style.backgroundColor = "red";
+        numberDisplay.textContent = secretNumber;
+      }
+    } else if (userGuess < secretNumber) {
+      // inform the user if the guess is lower than the random number
+      message.textContent = "📉 Too low, try again";
+      // decreasing the score by 1
+      score--;
+      scoreDisplay.textContent = score;
+      if (score === 0) {
+        score = 0;
+        message.textContent =
+          "😭 You lost the game, the number was " + secretNumber;
+        scoreDisplay.textContent = 0;
+        document.body.style.backgroundColor = "red";
+        score = 0;
+      }
+    } else if (userGuess === secretNumber) {
+      // inform the user if the guess is correct
+      message.textContent = "🎉 Congratulations, you guessed the number!";
+      // changing the background color of the body
+      document.body.style.backgroundColor = "#60b347";
+      numberDisplay.textContent = secretNumber;
+      // saving the high score if the current score is higher than the high score
+      if (score > highScore) {
+        highScore = score;
+        highScoreDisplay.textContent = highScore;
+      }
+    }
+  }
+});
+
+// adding event listener to again button
+againBtn.addEventListener("click", () => {
+  // resetting the score and high score
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // changing the background color of the body
+  document.body.style.backgroundColor = "#222";
+  // resetting the message and score display
+  message.textContent = "Start guessing...";
+  numberDisplay.textContent = "?";
+  scoreDisplay.textContent = score;
+  guessInput.value = "";
+});
